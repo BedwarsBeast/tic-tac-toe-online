2f521d2f5681590d22abb4d650bf51ed57f4dc0b
Bedwars beast <lichessplayer561@gmail.com>
2025-09-22 17:56:14 +0330

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,30 @@
 import socket
 import threading
 
-SERVER = "tic-tac-toe-online-r5kl.onrender.com" 
+SERVER = "localhost"
 PORT = 5555
 
+board = [' ']*9
+
+def print_board():
+    print(f"\n {board[0]} | {board[1]} | {board[2]} ")
+    print("---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]} \n")
+
 def listen(sock):
+    global board
     while True:
         try:
             data = sock.recv(1024).decode()
-            if not data:
-                break
-            print(data)
+            if not data: break
+            if data.startswith("BOARD"):
+                parts = data.split()
+                board = list(parts[1:10])
+                print_board()
+            else:
+                print(data)
         except:
             break
 
@@ -20,8 +34,9 @@ def main():
     threading.Thread(target=listen, args=(sock,), daemon=True).start()
 
     while True:
-        msg = input("Enter cell (0-8): ")
-        sock.sendall(msg.encode())
+        move = input("Enter cell (0-8): ")
+        sock.sendall(move.encode())
 
 if __name__ == "__main__":
     main()
+
